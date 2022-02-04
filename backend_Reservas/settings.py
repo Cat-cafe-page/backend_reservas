d9c7738cb24e83015027e701183c0a690d088c0e
Daniela Medina <87763778+daniela904@users.noreply.github.com>
2022-02-04 14:59:54 -0500

    Update settings.py

--- backend_Reservas/settings.py
+++ backend_Reservas/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'backend_Reservas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
-        'NAME'     : 'book_visit',
-        'USER'     : 'postgres',
-        'PASSWORD' : 'password',
-        'HOST'     : 'localhost',
-        'PORT'     : '5432'       
+        'NAME'     : 'database_name',
+        'USER'     : 'username',
+        'PASSWORD' : 'user_password',
+        'HOST'     : 'host',
+        'PORT'     : 'port_number'       
     }
 }
 
